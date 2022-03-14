f3b99bc8779bef4ce60d33904155e7d88c9efc42
J0bot <jojo64364@gmail.com>
2022-03-14 10:48:02 +0100

    début des tests

---
+++ arduino_code/src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+
+#include "secrets.h"
+
+
+//Tout ce qui concerne le wifi
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;      // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;     // le status du wifi
+
+//Variable client de la classe WiFiClient
+WiFiClient client;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(2, OUTPUT);
+
+    // Ici on va essayer de se connecter au wifi
+    while ( status != WL_CONNECTED) {
+        status = WiFi.begin(ssid, pass);
+        Serial.println("Trying connection to wifi...");
+        // wait 5 seconds for connection to the wifi
+        delay(5000);
+    }
+
+    IPAddress ardIpAddress = WiFi.localIP();
+    Serial.println(ardIpAddress);
+}
+
+void ledFadeInAndOut()
+{
+
+    for(int i=0; i<255; i++){
+    analogWrite(2, i);
+    delay(5);
+    }
+    for(int i=255; i>0; i--){
+        analogWrite(2, i);
+        delay(5);
+    }
+
+}
+
+void loop()
+{
+    ledFadeInAndOut();
+}
---
+++ arduino_code/src/secrets.h
@@ -0,0 +1,4 @@
+//#define SECRET_SSID "ZyXEL2DCB00"
+#define SECRET_SSID "NETGEAR96"
+//#define SECRET_PASS "1F81BFE86C63C"
+#define SECRET_PASS "freshviolin032"
